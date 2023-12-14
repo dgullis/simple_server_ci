e10c5d1e1b0460121f117f3d18768003f8cda4f6
dan gullis <daniel.gullis1@gmail.com>
2023-12-14 13:18:30 +0000

    Added /name route

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,3 +8,8 @@ def test_get_bye(web_client):
     assert response.status_code == 200
     assert response.data.decode("utf-8") == "Goodbye, world!"
 
+def test_get_name(web_client):
+    response = web_client.get("/name")
+    assert response.status_code == 200
+    assert response.data.decode("utf-8") == "Hello, dan"
+
